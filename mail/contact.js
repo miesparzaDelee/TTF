3d69ec6a1c11892b6769c0078a64879aafa1187f
Miguel Esparza <42750822+miesparzaDelee@users.noreply.github.com>
2023-01-14 15:22:17 -0600

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "test.php",
+                url: "mail/test.php",
                 type: "POST",
                 data: {
                     name: name,
